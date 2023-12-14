f6256d7eb9ff0ccc8f5e16fe80796c8320090d49
Jonathan Aires <airesjonathan123@gmail.com>
2023-12-13 21:21:39 -0300

    Counting movements

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,7 +7,8 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece{
 	
 	private Color color;
-
+	private int moveCount;
+	
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
@@ -19,6 +20,18 @@ public abstract class ChessPiece extends Piece{
 	
 	public Color getColor() {
 		return color;
+	}
+	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	protected void increaseMoveCount(){
+		moveCount++;
+	}
+	
+	protected void decreaseMoveCount(){
+		moveCount--;
 	}
 	
 	protected boolean isThereOpponentPiece(Position position) {
